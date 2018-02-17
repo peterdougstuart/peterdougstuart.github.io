7ed57ae6962e2de455d4573601acdcb2e826f5fd
peterdougstuart <peter.stuart@gmail.com>
2018-02-17 22:47:58 +0000

    updated contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -39,12 +39,12 @@ $(function() {
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        error: function() {
+        error: function(jqXHR, textStatus, errorThrown) {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later! " + errorThrown));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
